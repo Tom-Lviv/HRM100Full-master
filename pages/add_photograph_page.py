b96805d03a84b47327a72be96480426b4c1e7529
Tom-Lviv <krotsya@gmail.com>
2020-03-16 22:55:04 -0400

    test2

--- pages/add_photograph_page.py
+++ pages/add_photograph_page.py
@@ -9,6 +9,9 @@ class AddPhotographPage:
     def emloyee_picture(self):
         self.driver.find_element_by_id('empPic').click()
 
+
+        ##########
+
     def chose_file(self, file_path):
         self.driver.find_element_by_name('photofile').send_keys(file_path)
 
